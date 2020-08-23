cb26031d4eec52a64bdfe34480c920d109210b2a
Kirill1504 <kirik072007@gmail.com>
2020-08-23 13:27:12 +0300

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,18 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat catbean = (Cat) applicationContext.getBean("cat");
+        Cat catbean2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean == bean2);
+        System.out.println(catbean == catbean2);
+    }
+}
